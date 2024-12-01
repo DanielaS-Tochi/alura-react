9e6ba712598cb32d2629462b82182aa4a243e9f9
DanielaS-Tochi <danielastochi@gmail.com>
2024-12-01 18:03:02 -0300

    Components

---
+++ src/components/Formulario/Formulario.jsx
@@ -0,0 +1,13 @@
+import "./Formulario.css";
+
+const Formulario = () => {
+  return (
+    <section className="formulario">
+      <form>
+        <h2>Completa el formulario para crear el colaborador</h2>
+      </form>
+    </section>
+  );
+};
+
+export default Formulario;
