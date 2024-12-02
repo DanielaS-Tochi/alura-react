4bedf738aedc0b3d678deef342eaaf46537a64a9
DanielaS-Tochi <danielastochi@gmail.com>
2024-12-01 22:04:35 -0300

    Formulario

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -1,10 +1,15 @@
 import "./Formulario.css";
+import CampoTexto from "../CampoTexto";
+// Aquí iría indexedDB.jsx pero no es necesario colocar el nombre del archivo
 
 const Formulario = () => {
   return (
     <section className="formulario">
       <form>
         <h2>Completa el formulario para crear el colaborador</h2>
+        <CampoTexto />
+        <CampoTexto />
+        <CampoTexto />
       </form>
     </section>
   );
